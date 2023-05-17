412c1dbf5173323362980de0e74e8d0436b16009
NohelAdi <90423948+NohelAdi@users.noreply.github.com>
2023-05-16 23:05:02 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,49 +1,66 @@
-import sqlite3
+
+import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__, template_folder='week11')
-app.secret_key = "key"
+app = Flask(__name__, template_folder='temp')
+Bootstrap(app)
+app.secret_key = "secret key"
 
-conn = sqlite3.connect('employees.db')
+conn = mysql.connector.connect(
+	host='localhost',
+	user='final1',
+	password='nohel1',
+	database= 'Students'
+)
+
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS employees (
-        EMPID INTEGER PRIMARY KEY AUTOINCREMENT,
-        EMPName TEXT,
-        EMPGender TEXT,
-        EMPPhone TEXT,
-        EMPBdate DATE)''')
-print("Table has been created")
+        StudentID INT AUTO_INCREMENT PRIMARY KEY,
+        StudentName VARCHAR(200),
+        StudentBDate VARCHAR(200),
+        StudentAddress VARCHAR(200))''')
+print("Table created")
 conn.close()
 
 @app.route('/')
 def home():
-    return render_template('Home_Page.html')
+    return render_template('studenthomepage.html')
 
-@app.route('/registration', methods=['GET', 'POST'])
-def registration():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
-        empname = request.form['EMPName']
-        empgender = request.form['EMPGender']
-        empphone = request.form['EMPPhone']
-        empbdate = request.form['EMPBdate']
+        StudentName = request.form['StudentName']
+        StudentBDate = request.form['StudentBDate']
+        StudentAddress = request.form['StudentAddress']
 
-        conn = sqlite3.connect('employees.db')
+        conn = mysql.connector.connect(
+		host='localhost',
+		user='final1',
+		password='nohel1',
+		database= 'Students'
+)
         c = conn.cursor()
-        c.execute("INSERT INTO employees (EMPName, EMPGender, EMPPhone, EMPBdate) VALUES (?, ?, ?, ?)" , (empname, empgender, empphone, empbdate))
+        c.execute("INSERT INTO Students (StudentName, StudentBDate, StudentAddress) VALUES ('{0}','{1}','{2}','{3}')" , (StudentName, StudentBDate, StudentAddress))
         conn.commit()
         conn.close()
 
-        return redirect(url_for('information_page'))
+        return redirect(url_for('information'))
     
-    return render_template('registration_page.html')
+    return render_template('register.html')
 
 @app.route('/information')
 def information():
-    with sqlite3.connect('employees.db') as con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM employees")
-        rows = cur.fetchall()
-    return render_template('information_page.html', rows=rows)
+	conn = mysql.connector.connect(
+		host='localhost',
+		user='final1',
+		password='nohel1',
+		database= 'Students'
+)
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM Students")
+	rows = cur.fetchall()
+	return render_template('information.html', rows=rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
